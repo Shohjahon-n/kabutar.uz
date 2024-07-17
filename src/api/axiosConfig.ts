b5fd66d4fa898b3f4723ced729eeac8b8d517dc4
Shohjahon-n <pazvat56@gmail.com>
2024-07-17 14:23:41 +0500

    test

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const postUser = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://localhost:1111/',
     headers: {
         'Content-Type': 'application/json',
     }
